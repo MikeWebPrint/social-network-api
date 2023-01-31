fe4f9391b2e7105db39cec975137b9e9501f8e82
Mike Johnson <mike@mapleprintservices.com>
2023-01-31 18:31:06 -0500

    progress on models routes and server

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: String,
+      default: new Obl
+    },
+
+  }
+)
+
+const thoughtSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    }
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+
+module.exports = thoughtSchema;
